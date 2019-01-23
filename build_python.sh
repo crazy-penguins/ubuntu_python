304e5134e1647c6fb3da14fc324563a415166262
2ps <pshingavi@gmail.com>
2019-01-22 20:15:13 -0600

    Removed apt based installation of python since, duh, we're building it.

--- build_python.sh
+++ build_python.sh
@@ -7,12 +7,16 @@ apt-get -qq install wget zip gcc g++ libsqlite3-dev libbz2-dev \
   libffi-dev make libreadline-dev libz3-dev libfreetype6-dev \
   libncurses5-dev libncursesw5-dev tk-dev libgdbm-dev \
   libxml2-dev libxslt1-dev # libexpat1-dev  liblzma-dev
-apt-get -qq install python python-pip
-pip -q install awscli
+# apt-get -qq install python python-pip
+# pip -q install awscli
 url="https://www.python.org/ftp/python/${version}/Python-${version}.tgz"
 filename="python-${version}.tgz"
 wget -q $url -O $filename
 tar xzf $filename --strip-components=1
 rm $filename
-./configure --enable-optimizations
+./configure
 make
+make altinstall
+python --version
+pip --version
+pip install awscli
