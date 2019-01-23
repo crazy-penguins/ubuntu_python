f20cc636cd52437e9572ffc0c69588cf73a5e1ce
2ps <pshingavi@gmail.com>
2019-01-22 20:31:45 -0600

    Added libdb dependency

--- build_python.sh
+++ build_python.sh
@@ -6,16 +6,18 @@ apt-get -qq update
 apt-get -qq install wget zip gcc g++ libsqlite3-dev libbz2-dev \
   libffi-dev make libreadline-dev libz3-dev libfreetype6-dev \
   libncurses5-dev libncursesw5-dev tk-dev libgdbm-dev \
-  libxml2-dev libxslt1-dev libssl-dev libgdbm-compat-dev
+  libxml2-dev libxslt1-dev libssl-dev libgdbm-compat-dev \
+  libdb-dev 
 url="https://www.python.org/ftp/python/${version}/Python-${version}.tgz"
 filename="python-${version}.tgz"
 wget -q $url -O $filename
 tar xzf $filename --strip-components=1
 rm $filename
 ./configure >/dev/null
-make
+make >/dev/null
 make altinstall >/dev/null
 echo $PATH
-/usr/local/bin/python --version
+which python2.7
+/usr/local/bin/python2.7 --version
 /usr/local/bin/pip --version
 /usr/local/bin/pip install awscli
