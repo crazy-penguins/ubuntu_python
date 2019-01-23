434ba601b2fa2d90438d7144a9ab361e7b5ec95e
2ps <pshingavi@gmail.com>
2019-01-22 22:20:02 -0600

    Let's try docker and see what we can get.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:bionic
+ENV container docker
+WORKDIR /work
+SHELL [ "/bin/bash", "-c" ]
+COPY NAME .
+COPY VERSION .
+COPY build_python.sh .
+RUN cat ./NAME > /etc/docker.conf \
+  && printf ":" >> /etc/docker.conf \
+  && cat ./VERSION >> /etc/docker.conf \
+  && cat /etc/docker.conf \
+  && mkdir -p /u \
+  && chmod 0755 build_python.sh
+RUN /work/build_python.sh \
+  && rm -rf VERSION NAME
--- build_python.sh
+++ build_python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=${1:-"3.6.7"}
+version=$(cat /work/VERSION)
 apt-get -qq update
 apt-get -qq install software-properties-common
 apt-get -qq update
@@ -8,7 +8,7 @@ apt-get -qq install wget zip gcc g++ libsqlite3-dev libbz2-dev \
   libncurses5-dev libncursesw5-dev tk-dev libgdbm-dev \
   libxml2-dev libxslt1-dev libssl-dev libgdbm-compat-dev \
   libdb-dev python python-pip
-pip install awscli
+pip install -q awscli
 url="https://www.python.org/ftp/python/${version}/Python-${version}.tgz"
 mkdir -p /u/downloads
 filename="/u/downloads/python-${version}.tgz"
@@ -18,5 +18,13 @@ mkdir -p ${build_dir} && pushd ${build_dir}
 tar xzf $filename --strip-components=1
 ./configure --enable-optimizations >/dev/null
 make >/dev/null
-make altinstall >/dev/null
+zip -r -q -m /work/python-${version}.zip .
+rm -rf "$build_dir"
+rm -f "$filename"
+apt-get -qq remove wget zip gcc g++ libsqlite3-dev libbz2-dev \
+  libffi-dev make libreadline-dev libz3-dev libfreetype6-dev \
+  libncurses5-dev libncursesw5-dev tk-dev libgdbm-dev \
+  libxml2-dev libxslt1-dev libssl-dev libgdbm-compat-dev \
+  libdb-dev
+rm -rf /var/lib/apt/lists
 popd
