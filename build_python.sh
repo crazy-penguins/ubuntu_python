96c2b258fb717fbadd59aa14fcd594f060f855ba
2ps <pshingavi@gmail.com>
2019-01-22 20:46:46 -0600

    Chose the path of least resistance and installed python and pip from aws

--- build_python.sh
+++ build_python.sh
@@ -7,7 +7,8 @@ apt-get -qq install wget zip gcc g++ libsqlite3-dev libbz2-dev \
   libffi-dev make libreadline-dev libz3-dev libfreetype6-dev \
   libncurses5-dev libncursesw5-dev tk-dev libgdbm-dev \
   libxml2-dev libxslt1-dev libssl-dev libgdbm-compat-dev \
-  libdb-dev
+  libdb-dev python python-pip
+pip install awscli
 url="https://www.python.org/ftp/python/${version}/Python-${version}.tgz"
 filename="python-${version}.tgz"
 wget -q $url -O $filename
@@ -16,8 +17,3 @@ rm $filename
 ./configure >/dev/null
 make >/dev/null
 make altinstall >/dev/null
-which pip2
-which pip2.7
-/usr/local/bin/python2.7 --version
-/usr/local/bin/pip2.7 --version
-/usr/local/bin/pip2.7 install awscli
