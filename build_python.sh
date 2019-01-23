65c442cd90042515c838ec021d5ac51d1ae36577
2ps <pshingavi@gmail.com>
2019-01-22 19:43:40 -0600

    [OPS]  Added in ubuntu python builder

---
+++ build_python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+version=${1:-"3.6.7"}
+apt-get -qq update
+apt-get -qq install software-properties-common
+apt-get -qq update
+apt-get -qq install wget zip gcc g++ libsqlite3-dev libbz2-dev \
+  libffi-dev make libreadline-dev libz3-dev libfreetype6-dev \
+  libncurses5-dev libncursesw5-dev tk-dev libgdbm-dev \
+  libxml2-dev libxslt1-dev
+url="https://www.python.org/ftp/python/${version}/Python-${version}.tgz"
+filename="python-${version}.tgz"
+wget $url -O $filename
+tar xzf $filename --strip-components=1
+rm $filename
+./configure --enable-optimizations
+make
